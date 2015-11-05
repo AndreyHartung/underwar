4837ce39159e5f9ace9cb785a2ed4114604d26f8
Mosufe <andreyhartung@gmail.com>
2015-11-05 10:31:53 -0300

    dxfgdxg

---
+++ Underwar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Underwar.Startup))]
+namespace Underwar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
